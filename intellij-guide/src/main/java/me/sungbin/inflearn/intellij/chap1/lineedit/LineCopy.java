4813f7821313bb07d0f3ff4267a7f4109df51e5d
SungbinYang <ysb05222@naver.com>
2025-03-22 11:17:22 +0900

    라인 수정하기

---
+++ intellij-guide/src/main/java/me/sungbin/inflearn/intellij/chap1/lineedit/LineCopy.java
@@ -0,0 +1,8 @@
+package me.sungbin.inflearn.intellij.chap1.lineedit;
+
+public class LineCopy {
+
+    public void copyLine() {
+        System.out.println("현재 줄을 복사합니다.");
+    }
+}
---
+++ intellij-guide/src/main/java/me/sungbin/inflearn/intellij/chap1/lineedit/LineJoin.java
@@ -0,0 +1,12 @@
+package me.sungbin.inflearn.intellij.chap1.lineedit;
+
+public class LineJoin {
+
+    public String joinString() {
+        return "안녕하세요. IntelliJ 강의에 오신 것을 환영합니다.";
+    }
+
+    public String createQuery() {
+        return "SELECT * FROM MEMBER WHERE MEMBER.name = 'robert'";
+    }
+}
