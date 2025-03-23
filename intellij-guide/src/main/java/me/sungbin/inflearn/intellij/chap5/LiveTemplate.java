60be5d4fcec1400842de224e564d51cdbc6529f4
SungbinYang <ysb05222@naver.com>
2025-03-23 10:19:53 +0900

    Live Template

---
+++ intellij-guide/src/main/java/me/sungbin/inflearn/intellij/chap5/LiveTemplate.java
@@ -0,0 +1,17 @@
+package me.sungbin.inflearn.intellij.chap5;
+
+public class LiveTemplate {
+    public static void main(String[] args) {
+        String a = "a";
+        System.out.println();
+        if (a == null) {
+
+        }
+
+        if (a != null) {
+
+        }
+
+
+    }
+}
