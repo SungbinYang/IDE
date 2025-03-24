0bb04a1ca1e493e1d1b11b56ff77d4c283c2db6d
SungbinYang <ysb05222@naver.com>
2025-03-24 20:26:21 +0900

    다시  4줄로 수정

--- intellij-guide/src/main/java/me/sungbin/inflearn/intellij/chap8/Diff.java
+++ intellij-guide/src/main/java/me/sungbin/inflearn/intellij/chap8/Diff.java
@@ -5,5 +5,7 @@ public class Diff {
     public void diff() {
         System.out.println("diff-1");
         System.out.println("diff-1-1");
+        System.out.println("diff-2");
+        System.out.println("diff-3");
     }
 }
